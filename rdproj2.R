4ffb299662fe77c2aa1f56ca7c73d251689e8517
Shawn Rich <rich.shawn@gmail.com>
2015-08-20 14:23:24 -0400

    Even more exploration

--- rdproj2.R
+++ rdproj2.R
@@ -6,29 +6,82 @@
 
 # initializations
 
-library(lubridate)
-library(dplyr)
+# load libraries
+#library(lubridate)
+#library(dplyr)
 
-# set working_dir variable
-working_dir <- "~/Documents/courses/repdata/RepData_PeerAssessment2"
-
-# set working directory
-setwd(working_dir)
+# set working_dir
+#   change to match your computer when running this script
+#
+setwd("~/Documents/courses/repdata/RepData_PeerAssessment2")
 
 # read in the data file
-noaa <- read.csv("data/repdata_data_StormData.csv.bz2")
+noaa <- read.csv("data/repdata_data_StormData.csv.bz2",
+                 sep = ",",
+                 strip.white = TRUE,
+                 stringsAsFactors = FALSE)
 
-# include only events that had fatalities or injuries
-harmful <- noaa %>%
-           filter(FATALITIES > 0 | INJURIES > 0)
+# only include columns relevent to our questions
+noaa <- subset(noaa, 
+               select = c(EVTYPE,FATALITIES,INJURIES,
+                          PROPDMG,PROPDMGEXP,CROPDMG,CROPDMGEXP))
 
-# only looking at EVTYPE,FATALITIES, and INJURIES
-harmful <- subset(harmful, select = c(EVTYPE,FATALITIES,INJURIES))
+# upshift PROPDMGEXP and CROPDMGEXP columns
+noaa$PROPDMGEXP <- toupper(noaa$PROPDMGEXP)
+noaa$CROPDMGEXP <- toupper(noaa$CROPDMGEXP)
 
+# add columns for property and crop damage totals
+noaa$TOTPROPDMG <- 0
+noaa$TOTCROPDMG <- 0
+
+numbers <- as.character(1:9)
+
+# calculate property damage totals
+noaa$TOTPROPDMG[which(noaa$PROPDMGEXP %in% numbers)] <- 
+  noaa$PROPDMG[which(noaa$PROPDMGEXP %in% numbers)] * 1
+noaa$TOTPROPDMG[which(noaa$PROPDMGEXP == "H")] <- 
+  noaa$PROPDMG[which(noaa$PROPDMGEXP == "H")] * 100
+noaa$TOTPROPDMG[which(noaa$PROPDMGEXP == "K")] <- 
+  noaa$PROPDMG[which(noaa$PROPDMGEXP == "K")] * 1000
+noaa$TOTPROPDMG[which(noaa$PROPDMGEXP == "M")] <- 
+  noaa$PROPDMG[which(noaa$PROPDMGEXP == "M")] * 1000000
+noaa$TOTPROPDMG[which(noaa$PROPDMGEXP == "B")] <- 
+  noaa$PROPDMG[which(noaa$PROPDMGEXP == "B")] * 1000000000
+
+# calculate crop damage totals
+noaa$TOTCROPDMG[which(noaa$CROPDMGEXP %in% numbers)] <- 
+  noaa$CROPDMG[which(noaa$CROPDMGEXP %in% numbers)] * 1
+noaa$TOTCROPDMG[which(noaa$CROPDMGEXP == "H")] <- 
+  noaa$CROPDMG[which(noaa$CROPDMGEXP == "H")] * 100
+noaa$TOTCROPDMG[which(noaa$CROPDMGEXP == "K")] <- 
+  noaa$CROPDMG[which(noaa$CROPDMGEXP == "K")] * 1000
+noaa$TOTCROPDMG[which(noaa$CROPDMGEXP == "M")] <-
+  noaa$CROPDMG[which(noaa$CROPDMGEXP == "M")] * 1000000
+noaa$TOTCROPDMG[which(noaa$CROPDMGEXP == "B")] <- 
+  noaa$CROPDMG[which(noaa$CROPDMGEXP == "B")] * 1000000000
+
+max()
+
+
+
+
+df$n[which(df$m == "h")] <- df$n[which(df$m == "h")] * 100
+
+noaa$TOTPROPDMG 
 # --------------------------------------------------------------------
 
-head(harmful[with(harmful, order(-FATALITIES,-INJURIES)),])
-as.data.frame(table(harmful$EVTYPE))
+# 
+x <- df[!(df$m %in% c("a","c")),]
+noaa[noaa$PROPDMGEXP %in% c("+","-"," ","?"),]
+
+noaa[noaa$CROPDMGEXP %in% c("+","-"," ","?"),]
+x <- filter(noaa, PROPDMGEXP %in% c("+","-"," ","?"))
+
+x <- noaa[which(noaa$PROPDMGEXP %in% c("+","-"," ","?"))]
+unique(noaa$PROPDMGEXP)
+unique(noaa$CROPDMGEXP)
+CROPDMGEXP
+colnames(noaa)
 
 x<-
   subset(harmful$EVTYPE, harmful$FATALITIES %in% top20)
